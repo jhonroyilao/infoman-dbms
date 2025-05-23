8e449e330001628a10721c99c396b41387b75a25
jayrr <151696069+jhonroyilao@users.noreply.github.com>
2025-05-23 09:45:39 +0800

    Delete public directory

--- public/script.js
+++
@@ -1,6 +0,0 @@
-fetch('/api/users')
-  .then(res => res.json())
-  .then(data => {
-    const usersDiv = document.getElementById('users');
-    usersDiv.innerHTML = data.map(user => `<p>${user.name}</p>`).join('');
-  });
